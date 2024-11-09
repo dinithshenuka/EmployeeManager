d3e96f1b4549fe4db8195aa15adacf7ee2632ec6
Dinith <dinithshenuka@gmail.com>
2024-11-09 23:21:13 +0530

    create employee class

---
+++ src/main/java/lk/ac/iit/employeemanager/module/employee.java
@@ -0,0 +1,104 @@
+package lk.ac.iit.employeemanager.module;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+public class employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private long empId;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imgUrl;
+    @Column(nullable = false, updatable = false)
+    private String employeeCode;
+
+    public employee() {
+    }
+
+    public employee(long empId, String name, String email, String jobTitle, String phone, String imgUrl, String employeeCode) {
+        this.empId = empId;
+        this.name = name;
+        this.email = email;
+        this.jobTitle = jobTitle;
+        this.phone = phone;
+        this.imgUrl = imgUrl;
+        this.employeeCode = employeeCode;
+    }
+
+    public long getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(long empId) {
+        this.empId = empId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getEmployeeCode() {
+        return employeeCode;
+    }
+
+    public void setEmployeeCode(String employeeCode) {
+        this.employeeCode = employeeCode;
+    }
+
+    @Override
+    public String toString() {
+        return "employee{" +
+                "empId=" + empId +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", phone='" + phone + '\'' +
+                ", imgUrl='" + imgUrl + '\'' +
+                ", employeeCode='" + employeeCode + '\'' +
+                '}';
+    }
+}
+
+
